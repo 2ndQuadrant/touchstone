bf36c3a7a50de2c56a48696f4960d115c13e08d0
Mark Wong <mark@2ndQuadrant.com>
2016-12-10 07:56:54 -0800

    Add pseudorandom number generator
    This is the 64-bit version of Mersenne Twister pseudorandom number
    generator.  It is important to have a compatible license applied such
    that if the random number generators are modified, anyone can attempt to
    reproduce the results with the Touchstone project.

---
+++ src/mt19937-64.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2016 PostgreSQL Global Development Group
+ */
+
+#ifndef _MT19937_64_H_
+#define _MT19937_64_H_
+
+unsigned long long genrand64_int64(void);
+double genrand64_real2(void);
+
+#endif /* _MT19937_64_H_ */
