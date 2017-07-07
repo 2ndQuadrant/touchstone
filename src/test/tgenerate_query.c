2277253a29d4d12afeeae93e0fb4bb69f0af4a1a
Mark Wong <mark@2ndQuadrant.com>
2017-07-07 14:40:34 -0700

    Add query generation functions
    Support defining one line comments, transaction start and end keywords,
    and defining up to 9 variables.

---
+++ src/test/tgenerate_query.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 PostgreSQL Global Development Group
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "touchstone.h"
+
+int main(int argc, char *argv[])
+{
+	int rc;
+	struct query_t q;
+
+	if (argc != 4) {
+		fprintf(stderr, "usage: %s <config> <input> <output>\n", argv[0]);
+		return 1;
+	}
+	rc = load_query_parameters(argv[1], &q);
+	if (rc) {
+		fprintf(stderr, "load_query_parameters: %d\n", rc);
+		return 2;
+	}
+
+	q.id = 1;
+	q.vars = 1;
+	strcpy(q.var[0], "ONE");
+
+	return generate_query(argv[2], argv[3], &q);
+}
--- src/touchstone.h
+++ src/touchstone.h
@@ -1,14 +1,29 @@
 /*
- * Copyright 2016 PostgreSQL Global Development Group
+ * Copyright 2016-2017 PostgreSQL Global Development Group
  */
 
 #ifndef _TOUCHSTONE_H_
 #define _TOUCHSTONE_H_
 
 #define ALPHA_LEN 52
+#define BUFFER_LEN 64
+
+#define MAXVAR 9
+#define VAR_LEN 256
 
 #define int64 long int
 
+struct query_t {
+	int id;
+	int vars; /* Number of variables we have data for. */
+	char comment[BUFFER_LEN]; /* SQL one line comment prefix. */
+	char end[BUFFER_LEN]; /* End transaction. */
+	char start[BUFFER_LEN]; /* Start transaction */
+	/* This is currently hard coded to be able to hold up to 9 variables. */
+	char var[MAXVAR][VAR_LEN];
+};
+
+int generate_query(char *, char *, struct query_t *);
 void get_alpha(char *, int, int);
 int get_days(int);
 int64 getExponentialRand(int64, int64, double);
@@ -16,5 +31,6 @@ int64 getGaussianRand(int64, int64, double);
 int64 getPoissonRand(int64);
 int64 getrand(int64, int64);
 void init_genrand64(unsigned long long);
+int load_query_parameters(char *, struct query_t *);
 
 #endif /* _TOUCHSTONE_H_ */
